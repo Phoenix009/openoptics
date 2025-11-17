df19553eec54c99290d3e8a6329f7bcadd850971
Phoenix09 <jaideepmore307@gmail.com>
2025-11-17 10:50:44 +0000

    refactor ns3 backend

--- examples/routing_direct_perhop_ns3.py
+++ examples/routing_direct_perhop_ns3.py
@@ -24,20 +24,20 @@ def setup_echo_server_client(net):
     # Add internet stack to the terminals
 
     internet = ns.InternetStackHelper()
-    internet.Install(net.tors)
+    internet.Install(net.backend.tors)
 
     # We've got the "hardware" in place.  Now we need to add IP addresses.
     #
     ipv4 = ns.Ipv4AddressHelper()
     ipv4.SetBase(ns.Ipv4Address("10.1.1.0"), ns.Ipv4Mask("255.255.255.0"))
-    ipv4.Assign(net.tor_ports)
+    ipv4.Assign(net.backend.tor_ports)
 
     #
     # Create UDP echo server on node 0 and client on node 1
     port = 9  # Discard port (RFC 863)
 
     echoServer = ns.UdpEchoServerHelper(port)
-    serverApps = echoServer.Install(net.tors.Get(0))
+    serverApps = echoServer.Install(net.backend.tors.Get(0))
     serverApps.Start(ns.Seconds(1))
     serverApps.Stop(ns.Seconds(20))
 
@@ -46,7 +46,7 @@ def setup_echo_server_client(net):
     echoClient.SetAttribute("Interval", ns.TimeValue(ns.Seconds(1)))
     echoClient.SetAttribute("PacketSize", ns.UintegerValue(1024))
 
-    clientApps = echoClient.Install(ns.NodeContainer(net.tors.Get(1)))
+    clientApps = echoClient.Install(ns.NodeContainer(net.backend.tors.Get(1)))
     clientApps.Start(ns.Seconds(2))
     clientApps.Stop(ns.Seconds(20))
 
